732f1f698c16af902e5dc05fbf7c2db494712919
Glenn Ye <glenn.ye38@gmail.com>
2020-11-28 22:21:19 -0500

    Added preamble

---
+++ doodlejump.s
@@ -0,0 +1,39 @@
+#####################################################################
+#
+# CSCB58 Fall 2020 Assembly Final Project
+# University of Toronto, Scarborough
+#
+# Student: Glenn Qing Yuan Ye, 1006102977
+#
+# Bitmap Display Configuration:
+# - Unit width in pixels: 16					     
+# - Unit height in pixels: 16
+# - Display width in pixels: 512
+# - Display height in pixels: 512
+# - Base Address for Display: 0x10008000 ($gp)
+#
+# Which milestone is reached in this submission?
+# (See the assignment handout for descriptions of the milestones)
+# - Milestone 1/2/3/4/5 (choose the one the applies)
+#
+# Which approved additional features have been implemented?
+# (See the assignment handout for the list of additional features)
+# 1. (fill in the feature, if any)
+# 2. (fill in the feature, if any)
+# 3. (fill in the feature, if any)
+# ... (add more if necessary)
+#
+# Link to video demonstration for final submission:
+# - (insert YouTube / MyMedia / other URL here). 
+#
+# Any additional information that the TA needs to know:
+# - (write here, if any)
+#
+#####################################################################
+
+.data
+
+.text
+
+main:
+	
